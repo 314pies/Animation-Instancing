c65650f62b4bd52d92b240875738750f323b058c
314pie <ian121363@gmail.com>
2019-10-12 18:55:14 +0800

    Update unity version to 2018.4.10f1

---
+++ Assets/TEssttTemmp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEssttTemmp : MonoBehaviour
+{
+
+    private AnimationInstancing.AnimationInstancing instancing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instancing = GetComponent<AnimationInstancing.AnimationInstancing>();
+    }
+
+    public int index = 1;
+    public float crossFadeDur = 0.3f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+		    instancing.CrossFade(index, crossFadeDur);
+        if (Input.GetKeyDown(KeyCode.S))
+            instancing.PlayAnimation(index);
+    }
+}
