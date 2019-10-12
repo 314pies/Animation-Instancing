f46a5d7dab026432002288db2d03650e06afca24
314pie <ian121363@gmail.com>
2019-10-12 20:22:16 +0800

    Update testing assets. Import fps display Add random animations playing

---
+++ Assets/FPSDisplay.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+namespace Dazad.Utilities
+{
+    public class FPSDisplay : MonoBehaviour
+    {
+        float deltaTime = 0.0f;
+        public int FontSize = 5;
+        public Color TextColor = Color.green;
+
+        //public Text FPSText;
+
+        void Update()
+        {
+            deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        }
+
+        void OnGUI()
+        {
+            int w = Screen.width, h = Screen.height;
+
+            GUIStyle style = new GUIStyle();
+
+            Rect rect = new Rect(0, 0, w, h * 2 / 100);
+            style.alignment = TextAnchor.UpperLeft;
+            style.fontSize = h * FontSize / 100;
+            style.normal.textColor = TextColor;
+            float msec = deltaTime * 1000.0f;
+            float fps = 1.0f / deltaTime;
+            string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+            GUI.Label(rect, text, style);
+        }
+    }
+}
---
+++ Assets/RandomAnimations.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimations : MonoBehaviour
+{
+    public Vector2 RandomTimeGap = new Vector2(1,5);
+    private AnimationInstancing.AnimationInstancing instancing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instancing = GetComponent<AnimationInstancing.AnimationInstancing>();
+        Invoke("RndAnim",Random.Range(RandomTimeGap.x, RandomTimeGap.y));
+    }
+
+    public int AnimationCount = 2;
+    void RndAnim()
+    {
+        index = Random.Range(0, AnimationCount);
+        instancing.CrossFade(index, crossFadeDur);
+        Invoke("RndAnim", Random.Range(RandomTimeGap.x,RandomTimeGap.y));
+    }
+
+    public int index = 1;
+    public float crossFadeDur = 0.3f;
+
+}
